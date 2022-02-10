34fd7efceb733cb9e22dfb30ce82b73cf947392a
Kristian <kristian.arambasic@outlook.com>
2022-02-10 22:10:00 +0000

    copying production files to github

--- blog/models.py
+++ blog/models.py
@@ -40,7 +40,7 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
-    
+
 
 class RecipePart (models.Model):
     title = models.CharField(max_length=50, blank=True)
