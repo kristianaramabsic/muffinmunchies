4b7e5d5c27a8391dea049353d9dfa38fbd8b37a0
Kristian Arambasic <kristianarambasic@Kristians-MacBook-Pro.local>
2022-03-27 17:37:31 +0100

    created robotstxt template and urlconfig

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 from .views import HomePageView, RecipeView, AboutView, contactView, SubscriptionView, SubscriptionSuccessView, FoodCategoryView, AllCategoriesView
 
 urlpatterns = [
@@ -10,4 +11,5 @@ urlpatterns = [
     path ('subscription-success/', SubscriptionSuccessView.as_view(), name='subscription_success'),
     path ('<category_url>/', FoodCategoryView.as_view(), name='food_category'),
     path ('<str:category>/<title_slug>/', RecipeView.as_view(), name='recipe'),
+    path ("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
